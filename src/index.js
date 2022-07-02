e85f973c2e1712b3e05973e58fc061abbc645db7
wkchiuea <103587188+wkchiuea@users.noreply.github.com>
2022-07-02 23:03:39 +0800

    add redux store and provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from "react-redux";
+import { configureStore } from "redux";
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import 'tachyons'; 
+import { searchRobots } from './reducers';
+import 'tachyons';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const store = configureStore(rootReducer);
+const store = configureStore(searchRobots)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 // registerServiceWorker();
